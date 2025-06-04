47485d3e877672a86d1c2eb85b44cef058e436b5
shivendra <guptashivendra697@gmail.com>
2025-06-04 19:13:27 +0000

    datatype

---
+++ 01_basic/dataType.js
@@ -0,0 +1,2 @@
+"use strict";//treat all js code as a newer version
+//alert(8+8) run in browse not node node js
---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const accountId=12345678//this is use to constant value
+let accoundEmail="shiva@google.com"
+var accountPassoward=2344485;//this is not use bez of block scope and function scope
+accountCitiy="maharshtra"// this is not good to use
+console.log(accoundEmail);
+console.table([accoundEmail,accountCitiy,accountId,accountPassoward])
+
