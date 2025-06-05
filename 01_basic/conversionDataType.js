2f09a3a27d8d17dc3597b60000650ce07832233f
shivendra <guptashivendra697@gmail.com>
2025-06-05 07:22:35 +0000

    coversion

---
+++ 01_basic/conversionDataType.js
@@ -0,0 +1,23 @@
+// let score=33
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// let age=123
+// let ageInNumber=Number(age)
+// console.log(ageInNumber)
+// console.log(typeof(ageInNumber))
+// let temp=null
+// let temInNumber=Number(temp)
+// console.log(temInNumber);
+// console.log(typeof(temInNumber))
+// let booleanLoggedIn=""
+// let onlyboolean= Boolean(booleanLoggedIn)
+// console.log(onlyboolean)
+// console.log(typeof(onlyboolean))
+let name="shiva"
+let valueInString=String(name)
+console.log(valueInString)
+console.log(typeof(valueInString))
+let value="fjlk43"
+let nagValue=-value
+console.log(nagValue)
+console.log(typeof(nagValue))
